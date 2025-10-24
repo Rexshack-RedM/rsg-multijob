4ee9166d65e649836a271b4fdaa0beba846af490
RexShack <rexshackuk@gmail.com>
2025-10-24 06:44:49 +0100

    add useful exports

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rsg-multijob'
-version '2.0.3'
+version '2.0.4'
 
 shared_scripts { 
     '@ox_lib/init.lua',
@@ -11,13 +11,15 @@ shared_scripts {
 }
 
 client_scripts { 
-    'client/client.lua'
+    'client/client.lua',
+    'client/exports.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/server.lua',
-    'server/versionchecker.lua'
+    'server/versionchecker.lua',
+    'server/exports.lua'
 }
 
 files{
